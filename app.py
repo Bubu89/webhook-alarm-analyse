b44925cb783df31b843e73975b920df244d5aa91
Bubu89 <parkplatz.hallwang@gmail.com>
2025-06-21 15:32:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -164,3 +164,6 @@ def update_settings():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+# TEMPORÄR ZUM TESTEN
+sende_email("Test-Alarm", "Dies ist ein manueller Test.")
