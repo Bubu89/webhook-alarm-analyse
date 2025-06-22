5d375a1fdfaf169f04c150106a8aff18ad304625
Bubu89 <parkplatz.hallwang@gmail.com>
2025-06-22 12:15:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ def dashboard():
         einstellungs_info="",
         fehlerhafte_eintraege=fehlerhafte_eintraege
     )
-    if __name__ == "__main__":
+
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
-
