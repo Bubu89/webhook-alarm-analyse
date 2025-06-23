49fa02f50543465e237b66862020f602eb9fe480
Bubu89 <parkplatz.hallwang@gmail.com>
2025-06-23 17:41:34 +0200

    Update uploader.py

--- uploader.py
+++ uploader.py
@@ -1,28 +1,28 @@
-import git
 import os
+import git
 import shutil
-from datetime import datetime
+import datetime
 
-# Repo-Konfiguration
-# Sichere Konfiguration über Umgebungsvariablen
+# Token + URL sicher über Umgebungsvariablen
 TOKEN = os.getenv("GITHUB_TOKEN")
 REPO_URL = os.getenv("REPO_URL")
 REPO_AUTH_URL = REPO_URL.replace("https://", f"https://{TOKEN}@")
 LOCAL_REPO_PATH = "/tmp/webhook-alarm-repo"
 
-
 def git_upload(filename, local_source_path):
-    # Neues oder vorhandenes Repo klonen
+    # ggf. altes Repo löschen
     if os.path.exists(LOCAL_REPO_PATH):
         shutil.rmtree(LOCAL_REPO_PATH)
-    repo = git.Repo.clone_from(REPO_URL, LOCAL_REPO_PATH)
+
+    # Repo klonen – SICHER!
+    repo = git.Repo.clone_from(REPO_AUTH_URL, LOCAL_REPO_PATH)
 
     # Datei kopieren
     full_path = os.path.join(LOCAL_REPO_PATH, filename)
-    shutil.copy(local_source_path, full_path)
+    shutil.copy(os.path.join(local_source_path, filename), full_path)
 
-    # Commit und Push
+    # Commit & Push
     repo.git.add(filename)
-    repo.index.commit(f"Upload {filename} – {datetime.now().isoformat()}")
+    repo.index.commit(f"Upload {filename} – {datetime.datetime.now()}")
     origin = repo.remote(name='origin')
     origin.push()
